fb59564a3c389fe7480a2c95b1866ff04c7e5772
r-3than <ethan.gregory.ray@gmail.com>
2021-01-18 14:14:55 +0000

    bootstrap &jumbo added

--- testFlask.py
+++ testFlask.py
@@ -4,12 +4,11 @@ from flask import Flask, render_template
 app = Flask(__name__)
 @app.route("/")
 def hello():
-    return render_template("antiProcas.html")
+    return render_template("this.html")
 
 @app.route("/test/<testname>")
 def test(testname):
     return testname
-
 
 if __name__ == '__main__':
      app.run(port=5002,debug=True)
