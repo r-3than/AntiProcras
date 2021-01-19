4809d99ddb6269264e1b64389ee04929f4ca8edf
Ethan Ray <ethan.gregory.ray@gmail.com>
2021-01-19 11:05:15 +0000

    new code and development (gui)

--- testFlask.py
+++ testFlask.py
@@ -1,18 +1,38 @@
-from flask import Flask
-from flask import Flask, render_template
+from flask import Flask, render_template ,redirect
 
 app = Flask(__name__)
 @app.route("/")
-def hello():
-    return render_template("this.html")
+def index():
+    f = open("blockedpages.txt").read().split("\n")
+    return render_template("index.html",webpages=f)
 
-@app.route("/test/<testname>")
-def test(testname):
-    f = open("./test.txt","w")
-    f.write(testname)
-    f.close()
-    f=open("./test.txt","r").read()
-    return f
+@app.route("/remove/<webname>")
+def removeWeb(webname):
+    f = open("blockedpages.txt").read().split("\n")
+    try:f.remove(webname)
+    except: print("Webpage not found")
+    outdata = ""
+    for item in f:
+        outdata = outdata+item+"\n"
+    outdata = outdata[:-1]
+    Out = open("blockedpages.txt","w")
+    Out.write(outdata)
+    Out.close()
+    return redirect("/websites/")
+
+@app.route("/websites/")
+def websites():
+    f = open("blockedpages.txt").read().split("\n")
+    return render_template("websites.html",webpages=f)
+
+@app.route("/view/<page>")
+def viewPage(page):
+    page = "http://"+page
+    return redirect(page)
+
+@app.route("/add/")
+def addWeb():
+    return redirect("/")
 
 if __name__ == '__main__':
      app.run(port=5002,debug=True)
