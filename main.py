faaa5b67823f0c8b65813f76a80fe9c48d82884c
r-3than <ethan.gregory.ray@gmail.com>
2021-01-24 03:40:10 +0000

    moved to a class

--- main.py
+++ main.py
@@ -10,67 +10,138 @@ sites = open("blockedpages.txt").read().split("\n")
 times = open("state.txt").read().split("|")
 update = False
 
+class AntiPro:
+    def __init__(self):
+        self.sites = open("blockedpages.txt").read().split("\n")
+        self.times = open("state.txt").read().split("|")
+        Linux_host = "/etc/hosts"
+        Window_host = """C:\Windows\System32\drivers\etc\hosts"""
+        if platform.system() == "Linux":
+            default_hoster = Linux_host
+        else:
+            default_hoster = Window_host
+        self.default_hoster = default_hoster
+        self.update = False
+        self.running = True
+    def block_websites(self):
+
+        self.update = True
+        redirect = "127.0.0.1"
+        while self.running:
+            if self.update:
+                sites_to_block = self.sites
+                start_hour = int(self.times[0])
+                end_hour = int(self.times[1])
+                print("update!")
+                self.update = False
+            if is_time_between(datetime.time(start_hour,0), datetime.time(end_hour,00)): 
+                print("BLOCK!")
+                print(sites_to_block)
+                with open(self.default_hoster, 'r+') as hostfile:
+                    hosts = hostfile.read()
+                    for site in  sites_to_block:
+                        if site != '':
+                            if site not in hosts:
+                                hostfile.write(redirect+'\t'+site+'\n')
+            else:
+                print("UNLOCK")
+                with open(self.default_hoster, 'r+') as hostfile:
+                    hosts = hostfile.readlines()
+                    hostfile.seek(0)
+                    for host in hosts:
+                        if not any(site in host for site in sites_to_block):
+                            hostfile.write(host)
+                    hostfile.truncate()
+            time.sleep(3)
+    def removePage(self,webname):
+        try:self.sites.remove(webname)
+        except: print("Webpage not found")
+        outdata = ""
+        for item in self.sites:
+            outdata = outdata+item+"\n"
+        outdata = outdata[:-1]
+        Out = open("blockedpages.txt","w")
+        Out.write(outdata)
+        Out.close()
+        sites_to_block = [webname]
+
+        with open(self.default_hoster, 'r+') as hostfile:
+                    hosts = hostfile.readlines()
+                    hostfile.seek(0)
+                    for host in hosts:
+                        if not any(site in host for site in sites_to_block):
+                            hostfile.write(host)
+                    hostfile.truncate()
+        return self.sites
+    def addPage(self,webname):
+        self.sites.append(webname)
+        f = open("blockedpages.txt","a")
+        f.write("\n"+webname)
+        f.close()
+        self.update = True
+        return self.sites
+    def unlockall(self):
+        sites_to_block = self.sites
+        with open(self.default_hoster, 'r+') as hostfile:
+                    hosts = hostfile.readlines()
+                    hostfile.seek(0)
+                    for host in hosts:
+                        if not any(site in host for site in sites_to_block):
+                            hostfile.write(host)
+                    hostfile.truncate()
+    def setHours(self,start,end):
+        f = open("state.txt","w")
+        f.write(start+"|"+end+"|1")
+        f.close()
+        self.times = [start,end,1]
+        self.update = True
+    def getTimes(self):
+        return self.times
+    def getSites(self):
+        return self.sites
+    def on(self):
+        self.running = True
+        self.watcher = threading.Thread(target=self.block_websites).start()
+    def off(self):
+        self.running = False
+        self.update = False
+        self.unlockall()
+AP = AntiPro()
 app = Flask(__name__)
+
 @app.route("/")
 def index():
-    global sites
-    f = sites
+    f = AP.getSites()
     return render_template("index.html",webpages=f)
 
 @app.route("/remove/<webname>")
 def removeWeb(webname):
-    global update
-    global sites
-    f = sites
-    try:f.remove(webname)
-    except: print("Webpage not found")
-    outdata = ""
-    for item in f:
-        outdata = outdata+item+"\n"
-    outdata = outdata[:-1]
-    Out = open("blockedpages.txt","w")
-    Out.write(outdata)
-    Out.close()
-    removeSite(webname)
-    update = True
+    AP.removePage(webname)
     return redirect("/websites/")
 
 @app.route("/websites/",methods = ['POST', 'GET'])
 def websites():
-    global update
-    global sites
     if request.method == 'POST':
-        user = request.form['websitename']
-        sites.append(user)
-        f = open("blockedpages.txt","a")
-        f.write("\n"+user)
-        f.close()
-        update = True
-    f = sites
+        webname = request.form['websitename']
+        AP.addPage(webname)
+    f = AP.getSites()
     return render_template("websites.html",webpages=f)
 
 @app.route("/workhours/",methods = ['POST', 'GET'])
 def workhours():
-    global update
-    global times
-    global sites
     if request.method == 'POST':
         start = request.form['starthour']
         end = request.form['endhour']
-        f = open("state.txt","w")
-        f.write(start+"|"+end+"|1")
-        f.close()
-        times = [start,end,1]
-        update = True
-    f = sites
-    s = times
+        AP.setHours(start,end)
+    f = AP.getSites()
+    s = AP.getTimes()
     return render_template("workhours.html",webpages=f,startHour=s[0],endHour=s[1])
 
 @app.route("/controlpanel/",methods = ['POST', 'GET'])
 def controlpanel():
-    global sites
-    f = sites
-    return render_template("controlpanel.html",webpages=f)
+    f = AP.getSites()
+    keyCode = Ap.getCode()
+    return render_template("controlpanel.html",webpages=f,code=keyCode)
 
 
 @app.route("/view/<page>")
@@ -91,84 +162,6 @@ def is_time_between(begin_time, end_time, check_time=None):
     else:
         return check_time >= begin_time or check_time <= end_time
 
-
-
-def removeSite(website):
-    sites_to_block = [website]
-    Linux_host = "/etc/hosts"
-    Window_host = """C:\Windows\System32\drivers\etc\hosts"""
-    if platform.system() == "Linux":
-        default_hoster = Linux_host
-    else:
-        default_hoster = Window_host
-    with open(default_hoster, 'r+') as hostfile:
-                hosts = hostfile.readlines()
-                hostfile.seek(0)
-                for host in hosts:
-                    if not any(site in host for site in sites_to_block):
-                        hostfile.write(host)
-                hostfile.truncate()
-
-def unlockall():
-    global sites
-    Linux_host = "/etc/hosts"
-    Window_host = """C:\Windows\System32\drivers\etc\hosts"""
-    if platform.system() == "Linux":
-        default_hoster = Linux_host
-    else:
-        default_hoster = Window_host
-    sites_to_block = sites
-    with open(default_hoster, 'r+') as hostfile:
-                hosts = hostfile.readlines()
-                hostfile.seek(0)
-                for host in hosts:
-                    if not any(site in host for site in sites_to_block):
-                        hostfile.write(host)
-                hostfile.truncate()
-
-
-def block_websites():
-    global update
-    global sites
-    global times
-    update = True
-    Linux_host = "/etc/hosts"
-    Window_host = """C:\Windows\System32\drivers\etc\hosts"""
-    if platform.system() == "Linux":
-        default_hoster = Linux_host
-    else:
-        default_hoster = Window_host
-    redirect = "127.0.0.1"
-    while True:
-        if update:
-            sites_to_block = sites
-            start_hour = int(times[0])
-            end_hour = int(times[1])
-            print("update!")
-            update = False
-        if is_time_between(datetime.time(start_hour,0), datetime.time(end_hour,00)): 
-            print("BLOCK!")
-            print(sites_to_block)
-            with open(default_hoster, 'r+') as hostfile:
-                hosts = hostfile.read()
-                for site in  sites_to_block:
-                    if site != '':
-                        if site not in hosts:
-                            hostfile.write(redirect+'\t'+site+'\n')
-        else:
-            print("UNLOCK")
-            with open(default_hoster, 'r+') as hostfile:
-                hosts = hostfile.readlines()
-                hostfile.seek(0)
-                for host in hosts:
-                    if not any(site in host for site in sites_to_block):
-                        hostfile.write(host)
-                hostfile.truncate()
-        time.sleep(3)
-
-
-
-
 def is_admin():
     try:
         return ctypes.windll.shell32.IsUserAnAdmin()
@@ -179,7 +172,7 @@ def is_admin():
 if __name__ == '__main__':
     if platform.system() == "Windows":
         if is_admin():
-            watcher = threading.Thread(target=block_websites).start()
+            watcher = threading.Thread(target=AP.block_websites).start()
             app.run(port=5002,debug=False)
         else:
     # Re-run the program with admin rights
@@ -187,5 +180,5 @@ if __name__ == '__main__':
     else:
 
         #watcher = threading.Thread(target=block_websites).start()
-        watcher = threading.Thread(target=block_websites).start()
+        watcher = threading.Thread(target=AP.block_websites).start()
         app.run(port=5002,debug=False)
